f9a0287895a719ebddcb4cad3bbc1d6f5a539bf3
octavioislas <octavioislasm@gmail.com>
2023-12-13 18:36:45 +0000

    run

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css'
 function App() {
   return (
       <Router basename={process.env.PUBLIC_URL}>
+
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/quiz-questions/:quizId" element={<Quiz />} />
